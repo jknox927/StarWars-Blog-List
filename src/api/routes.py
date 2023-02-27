21fc1ca95817d230cb93e9ea44b38ff6e8a44259
jknox927 <joshua.knox927@gmail.com>
2023-02-27 03:36:35 +0000

    Working progress

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, People
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,39 @@ api = Blueprint('api', __name__)
 #     }
 
 #     return jsonify(response_body), 200
+
+@api.route('/', methods=['GET'])
+def home():
+    response_body = {
+        "message": "Welcome to StarWars REST API"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/people', methods=['GET'])
+def get_people():
+    return People.name
+
+@api.route('/people/<int:people_id>', methods=['GET'])
+def get_peopleDetails():
+    return "people_id"
+
+@api.route('/films', methods=['GET'])
+def get_films():
+    return "films"
+
+@api.route('/starships', methods=['GET'])
+def get_starships():
+    return "starships"
+
+@api.route('/vehicles', methods=['GET'])
+def get_vehicles():
+    return "vehicles"
+
+@api.route('/species', methods=['GET'])
+def get_species():
+    return "species"
+
+@api.route('/planets', methods=['GET'])
+def get_planets():
+    return "planets"
