7a81ea19d13f63020f9e4129e4a92f7491a695f2
jknox927 <joshua.knox927@gmail.com>
2023-02-28 02:14:21 +0000

    Api routing updates

--- src/api/models.py
+++ src/api/models.py
@@ -1,11 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    profile_pic = db.Column(db.String(128), nullable=True)
+    username = db.Column(db.String(128), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -19,5 +23,49 @@ class User(db.Model):
         }
 
 class People(db.Model):
+    __tablename__ = 'people'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256))
+    birth_year = db.Column(db.String(256))
+    eye_color = db.Column(db.String(256))
+    gender = db.Column(db.String(256))
+    hair_color = db.Column(db.String(256))
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    skin_color = db.Column(db.String(256))
+    homeworld = db.Column(db.String(256))
+    url = db.Column(db.String(256))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_edited = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # films_rel = db.relationship('Films', backref='films', uselist=True)
+    # films_id = db.Column(db.Integer, db.ForeignKey('films.id'))
+
+    # species_rel = db.relationship('Species', backref='species', uselist=True)
+    # species_id = db.Column(db.Integer, db.ForeignKey('species.id'))
+
+    # starships_rel = db.relationship('Starships', backref='starships', uselist=True)
+    # starships_id = db.Column(db.Integer, db.ForeignKey('starships.id'))
+
+    # vehicles_rel = db.relationship('Vehicles', backref='vehicles', uselist=True)
+    # vehicles_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))
+
+    def __repr__(self):
+        return f'<People {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "height": self.height,
+            "mass": self.mass,
+            "skin_color": self.skin_color,
+            "homeworld": self.homeworld,
+            "url": self.url,
+            "date_created": self.date_created,
+            "date_edited": self.date_edited
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,37 +1,38 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 from api.models import db, User, People
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
-
-# @api.route('/hello', methods=['POST', 'GET'])
-# def handle_hello():
-
-#     response_body = {
-#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-#     }
-
-#     return jsonify(response_body), 200
-
 @api.route('/', methods=['GET'])
-def home():
+def index():
     response_body = {
         "message": "Welcome to StarWars REST API"
     }
 
     return jsonify(response_body), 200
 
+@api.route('/login', methods=['POST'])
+def login():
+    req_username = request.json.get('username', None)
+    get_user: User = User.query.filter_by(username = req_username).first()
+    req_password = request.json.get('password', None)
+    if get_user.password != req_password:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity = username)
+    return jsonify(access_token = access_token)
+
 @api.route('/people', methods=['GET'])
 def get_people():
-    return People.name
+    req_people: People = People.query.all()
+    return jsonify(people = [people.serialize() for people in req_people])
 
 @api.route('/people/<int:people_id>', methods=['GET'])
-def get_peopleDetails():
-    return "people_id"
+def get_person():
+    person = People.query.filter_by(id=people_id).first()
+    return jsonify(person.serialize())
 
 @api.route('/films', methods=['GET'])
 def get_films():
